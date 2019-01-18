1f453c04ce98df36c11c698775adb7af1bc28598
Jasper Chan <jasperchan515@gmail.com>
2019-01-17 19:34:46 -0800

    PID Calibration E-1 S80

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -538,12 +538,15 @@
 #if ENABLED(PIDTEMPBED)
   //#define MIN_BED_POWER 0
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
+  #define  DEFAULT_bedKp 196.82
+  #define  DEFAULT_bedKi 20.70
+  #define  DEFAULT_bedKd 467.81
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  //#define  DEFAULT_bedKp 10.00
+  //#define  DEFAULT_bedKi .023
+  //#define  DEFAULT_bedKd 305.4
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
